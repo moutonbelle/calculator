4da585cafad58065662aca797a6636d73e999003
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-08-01 18:13:28 -0500

    Add keyboard support; refactor to allow keyboard and click events to share same fns

--- calc.js
+++ calc.js
@@ -27,24 +27,36 @@ function newNumber () {
     hasPoint = false;
 }
 
-function pressNumber (e) {    
+function num (newNum) {
     switch (readyFor) {
         case "operator":
             readyFor = "b";
         case "a":
         case "b":
             if (readyForNewNumber === true) display.textContent = "";
-            if (e.target.id === "point" && !hasPoint) {
-                display.textContent += (display.textContent === "") ? "0" + e.target.textContent : e.target.textContent;
-                hasPoint = true;
-            }
-            if (e.target.id !== "point") display.textContent += e.target.textContent;
+            display.textContent += newNum;
             readyForNewNumber = false;
             break;
-    }
+    } 
 }
 
-function pressEquals (e) {
+function point () {
+    switch (readyFor) {
+        case "operator":
+            readyFor = "b";
+        case "a":
+        case "b":
+            if (readyForNewNumber === true) display.textContent = "";
+            if (!hasPoint) {
+                display.textContent += (display.textContent === "") ? "0." : ".";
+                hasPoint = true;
+            }
+            readyForNewNumber = false;
+            break;
+    }    
+}
+
+function equals () {
     switch (readyFor) {
         case "a":
             if (operator && b) {
@@ -66,16 +78,16 @@ function pressEquals (e) {
     }
 }
 
-function pressOperator (e) {
+function setOperator (newOperator) {
     switch (readyFor) {
         case "a":
             a = display.textContent;
-            operator = e.target.textContent;
+            operator = newOperator;
             readyFor = "operator";
             newNumber();
             break;
         case "operator":
-            operator = e.target.textContent;
+            operator = newOperator;
             newNumber();
             break;
         case "b":
@@ -83,14 +95,14 @@ function pressOperator (e) {
             result = operate(Number(a), Number(b), operator);
             display.textContent = result;
             a = result;     
-            operator = e.target.textContent;
+            operator = newOperator;
             readyFor = "operator";
             newNumber();   
             break;
     }
 }
 
-function pressClear () {
+function clear () {
     display.textContent = 0;
     a = null;
     b = null;
@@ -101,7 +113,7 @@ function pressClear () {
     hasPoint = false;
 }
 
-function pressBackspace () {
+function backspace () {
     switch (readyFor) {
         case "a":
         case "b":
@@ -117,15 +129,46 @@ function pressBackspace () {
     }
 }
 
+function pressKey (e) {
+    switch (e.key) {
+        case "Backspace": backspace(); break;
+        case "c": clear(); break;
+
+        case "=": 
+        case "Enter": equals(); break;
+        
+        case "-": setOperator("-"); break;
+        case "+": setOperator("+"); break;
+        case "/": setOperator("/"); break;
+        case "*": setOperator("*"); break;
+
+        case "0": num(0); break;
+        case "1": num(1); break;
+        case "2": num(2); break;
+        case "3": num(3); break;
+        case "4": num(4); break;
+        case "5": num(5); break;
+        case "6": num(6); break;
+        case "7": num(7); break;
+        case "8": num(8); break;
+        case "9": num(9); break;
+    }
+}
+
 document.querySelectorAll("#number-pad button").forEach(button => {
-    button.addEventListener("click", pressNumber);
+    button.addEventListener("click", (e) => {
+        if (e.target.id === "point") point();
+        else num(Number(e.target.textContent));
+    });
 });
 
 document.querySelectorAll("#operators button").forEach(button => {
-    if (button.id === "equals") button.addEventListener("click", pressEquals);
-    else button.addEventListener("click", pressOperator);
+    if (button.id === "equals") button.addEventListener("click", equals);
+    else button.addEventListener("click", e => setOperator(e.target.textContent));
 });
 
-document.querySelector("#clear").addEventListener("click", pressClear);
+document.querySelector("#clear").addEventListener("click", clear);
 
-document.querySelector("#backspace").addEventListener("click", pressBackspace);
+document.querySelector("#backspace").addEventListener("click", backspace);
+
+window.addEventListener("keydown", pressKey);
