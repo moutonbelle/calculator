13d239c4e380d3b1ccf4f5f6fade8769c3a6575f
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-08-01 16:56:03 -0500

    Add calculator state and refactor functions based on state; supports calc on operator and serial =

--- calc.js
+++ calc.js
@@ -7,29 +7,92 @@ function operate (a, b, operator) {
     }
 }
 
+// Data
 let a = null;
 let b = null;
 let operator = null;
-let newNumber = true;
+let result = null;
+
+// Calculator state
+let readyForNewNumber = true;
+let readyFor = "a";
+
 const display = document.querySelector("#display p");
 
-function pressNumber (e) {
-    if (newNumber === true) display.textContent = "";
-    display.textContent += e.target.textContent;
-    newNumber = false;
+function pressNumber (e) {    
+    switch (readyFor) {
+        case "operator":
+            readyFor = "b";
+        case "a":
+        case "b":
+            if (readyForNewNumber === true) display.textContent = "";
+            display.textContent += e.target.textContent;
+            readyForNewNumber = false;
+            break;
+    }
 }
 
 function pressEquals (e) {
-    b = display.textContent;
-    display.textContent = operate(Number(a), Number(b), operator);
-    newNumber = true;
+    switch (readyFor) {
+        case "a":
+            if (operator && b) {
+                result = operate (Number(a), Number(b), operator);
+                display.textContent = result;
+                a = result;
+            }
+            break;
+        case "operator":
+            break;
+        case "b":
+            b = display.textContent;
+            result = operate(Number(a), Number(b), operator);
+            display.textContent = result;
+            a = result;
+            readyFor = "a";
+            readyForNewNumber = true;
+    }
 }
 
 function pressOperator (e) {
-    a = display.textContent;
-    operator = e.target.textContent;
-    newNumber = true;
+    switch (readyFor) {
+        case "a":
+            a = display.textContent;
+            operator = e.target.textContent;
+            readyFor = "operator";
+            readyForNewNumber = true;
+            break;
+        case "operator":
+            operator = e.target.textContent;
+            readyForNewNumber = true;
+            break;
+        case "b":
+            b = display.textContent;
+            result = operate(Number(a), Number(b), operator);
+            display.textContent = result;
+            a = result;     
+            readyFor = "operator";
+            readyForNewNumber = true;   
+            break;
+    }
 }
+
+
+//     if (aSet === false) {
+//         a = display.textContent;
+//         aSet = true;
+//         operator = e.target.textContent;
+//         readyForNewNumber = true;
+//     }
+//     else {
+//         b = display.textContent;
+//         bSet = true;
+//         result = operate(Number(a), Number(b), operator);
+//         display.textContent = result;
+//         a = result;
+//         operator = e.target.textContent;
+//         readyForNewNumber = true;        
+//     }
+// }
 
 document.querySelectorAll("#number-pad button").forEach(button => {
     button.addEventListener("click", pressNumber);
@@ -42,10 +105,22 @@ document.querySelectorAll("#operators button").forEach(button => {
 
 /*
 
-Hit operator
--- if b === null
-    take a, start entering b
-   else
-    calculate, take a, start entering b
+STATES
+
+-- Beginning
+-- Entering a
+    -- first number clears and adds number
+    -- subsequent numbers add number
+    -- operator sets operator and switches state to entering operator
+    -- equals does nothing
+    -- clear clears
+-- Entering operator
+    -- first number clears result and sets state to entering b and sets operator
+    -- operator changes operator and does nothing else
+    -- equals does nothing
+-- Entering b
+    -- operator calculates result and displays it, sets a to result, sets state to entering operator
+    -- numbers add number
+    -- equals calculates result and displays it, sets a to result, sets state to entering a
 
 */
