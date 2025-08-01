9b3a3e74241be3c5d2471781a880b3ab61b315fb
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-08-01 15:41:25 -0500

    Add operators and equals logic, happy path only

--- calc.js
+++ calc.js
@@ -10,11 +10,36 @@ function operate (a, b, operator) {
 let a = null;
 let b = null;
 let operator = null;
+let newNumber = true;
 const display = document.querySelector("#display p");
 
 document.querySelectorAll("#number-pad button").forEach(button => {
     button.addEventListener("click", e => {
+        if (newNumber === true) display.textContent = "";
         display.textContent += e.target.textContent;
+        newNumber = false;
     })
-})
+});
 
+document.querySelectorAll("#operators button").forEach(button => {
+    if (button.id === "equals") {
+        button.addEventListener("click", e => {
+            b = display.textContent;
+            display.textContent = operate (a, b, operator);
+        });
+    }
+    else {
+        button.addEventListener("click", e => {
+            a = display.textContent;
+            operator = e.target.textContent;
+            newNumber = true;
+        });
+    }
+});
+
+/*
+
+Just pushed operator
+next number push clears number and starts entering new number
+
+*/
