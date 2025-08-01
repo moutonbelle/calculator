381a3629ed53ba2398dc69d8bf1d63bc44802d52
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-08-01 16:01:36 -0500

    Refactor event handler functions into named fns for readability

--- calc.js
+++ calc.js
@@ -13,33 +13,39 @@ let operator = null;
 let newNumber = true;
 const display = document.querySelector("#display p");
 
+function pressNumber (e) {
+    if (newNumber === true) display.textContent = "";
+    display.textContent += e.target.textContent;
+    newNumber = false;
+}
+
+function pressEquals (e) {
+    b = display.textContent;
+    display.textContent = operate(Number(a), Number(b), operator);
+    newNumber = true;
+}
+
+function pressOperator (e) {
+    a = display.textContent;
+    operator = e.target.textContent;
+    newNumber = true;
+}
+
 document.querySelectorAll("#number-pad button").forEach(button => {
-    button.addEventListener("click", e => {
-        if (newNumber === true) display.textContent = "";
-        display.textContent += e.target.textContent;
-        newNumber = false;
-    })
+    button.addEventListener("click", pressNumber);
 });
 
 document.querySelectorAll("#operators button").forEach(button => {
-    if (button.id === "equals") {
-        button.addEventListener("click", e => {
-            b = display.textContent;
-            display.textContent = operate (a, b, operator);
-        });
-    }
-    else {
-        button.addEventListener("click", e => {
-            a = display.textContent;
-            operator = e.target.textContent;
-            newNumber = true;
-        });
-    }
+    if (button.id === "equals") button.addEventListener("click", pressEquals);
+    else button.addEventListener("click", pressOperator);
 });
 
 /*
 
-Just pushed operator
-next number push clears number and starts entering new number
+Hit operator
+-- if b === null
+    take a, start entering b
+   else
+    calculate, take a, start entering b
 
 */
