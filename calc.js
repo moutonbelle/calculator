69443b6bff9c360d3b6f02d3eaf80a1bb57ff5a4
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-08-01 15:21:52 -0500

    Add event listeners to numpad to update display with numbers

--- calc.js
+++ calc.js
@@ -6,3 +6,15 @@ function operate (a, b, operator) {
         case "/": return a/b;
     }
 }
+
+let a = null;
+let b = null;
+let operator = null;
+const display = document.querySelector("#display p");
+
+document.querySelectorAll("#number-pad button").forEach(button => {
+    button.addEventListener("click", e => {
+        display.textContent += e.target.textContent;
+    })
+})
+
